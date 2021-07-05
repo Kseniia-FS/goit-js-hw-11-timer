b5c47f3891bc03ee1a3e39a673207d481816ea05
Kseniia-FS <snabukraine@ukr.net>
2021-07-05 15:21:27 +0300

    Updates

--- src.8a360053.js
+++ src.8a360053.js
@@ -3,8 +3,8 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"leU0":[function(require,module,exports) {
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;class t{constructor({selector:t,targetDate:e}){this.targetDate=new Date(e),this.days=document.querySelector(`${t} .value[data-value="days"]`),this.hours=document.querySelector(`${t} .value[data-value="hours"]`),this.minutes=document.querySelector(`${t} .value[data-value="mins"]`),this.seconds=document.querySelector(`${t} .value[data-value="secs"]`)}pad(t){return t<10?`0${t}`:t}countDowm(){const t=new Date;this.createValueForSpan(t)}showTime(){setInterval(()=>this.countDowm(),1e3)}createValueForSpan(t){const e=this.targetDate-t;this.days.textContent=this.pad(Math.floor(e/864e5)),this.hours.textContent=this.pad(Math.floor(e%864e5/36e5)),this.minutes.textContent=this.pad(Math.floor(e%36e5/6e4)),this.seconds.textContent=this.pad(Math.floor(e%6e4/1e3))}}exports.default=t;
 },{}],"yCUT":[function(require,module,exports) {
-"use strict";var e=t(require("./plagin.js"));function t(e){return e&&e.__esModule?e:{default:e}}const r=new e.default({selector:"#timer-1",targetDate:"2021,07,17"});r.showTime();
+"use strict";var e=t(require("./plagin.js"));function t(e){return e&&e.__esModule?e:{default:e}}const r=new e.default({selector:"#timer-1",targetDate:"2022,07,17"});r.showTime();
 },{"./plagin.js":"leU0"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/timer.js");
 },{"./sass/main.scss":"clu1","./js/timer.js":"yCUT"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.5bbdd3ae.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.8a360053.js.map
